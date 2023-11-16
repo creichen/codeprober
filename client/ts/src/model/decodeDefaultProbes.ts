232a4875024a1f7dfb27e2b50fe18d59df693daa
Christoph Reichenbach <creichen@gmail.com>
2023-11-16 08:34:59 +0000

    Added missing file

---
+++ client/ts/src/model/decodeDefaultProbes.ts
@@ -0,0 +1,42 @@
+import WindowState from './WindowState';
+import { WindowStateData, WindowStateDataProbe, WindowStateDataAst, NestedWindows, WindowStateDataMinimized } from './WindowState';
+// import WindowStateDataProbe from './WindowState';
+// import WindowStateDataMinimized from './WindowState';
+
+const decodeDefaultProbes = (encoding: string) : WindowState[] => {
+  return (encoding.split(',')).flatMap((s : string) => {
+    const spec = decodeURI(s).split(':');
+    if (spec && spec.length > 1) {
+      const data : WindowStateDataProbe = {
+	type: 'probe',
+	locator: {
+	  result: {
+	    external: false,
+	    depth: 0,
+	    start: 0,
+	    end: 0,
+	    type: spec[0]
+	  },
+	  steps: [],
+	},
+	property: { name: spec[1] },
+	nested: {},
+	showDiagnostics: true,
+      };
+      const min_data : WindowStateDataMinimized = {
+	type: 'minimized-probe',
+	data
+      };
+      const result : WindowState = {
+	modalPos: { x: 0, y: 0 },
+	data: min_data,
+	isDefault: true
+      };
+      return [result];
+    }
+    console.log("Invalid spec:", spec, 'from encoding:', encoding);
+    return [];
+  })
+}
+
+export default decodeDefaultProbes;
