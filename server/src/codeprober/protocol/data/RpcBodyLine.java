dd9313739e53b614db17afe11f5994fd2cf65b58
Christoph Reichenbach <creichen@creichen.net>
2026-01-29 17:12:18 +0000

    Merged upstream

--- server/src/codeprober/protocol/data/RpcBodyLine.java
+++ server/src/codeprober/protocol/data/RpcBodyLine.java
@@ -138,7 +138,7 @@ public class RpcBodyLine implements codeprober.util.JsonUtil.ToJsonable {
       }
     case arr:
       try {
-        final java.util.List<RpcBodyLine> val = codeprober.util.JsonUtil.<RpcBodyLine>mapArr(obj.getJSONArray("value"), (arr2, idx2) -> RpcBodyLine.fromJSON(arr2.getJSONObject(idx2)));
+        final java.util.List<RpcBodyLine> val = codeprober.util.JsonUtil.<RpcBodyLine>mapArr(obj.getJSONArray("value"), (arr3, idx3) -> RpcBodyLine.fromJSON(arr3.getJSONObject(idx3)));
         return fromArr(val);
       } catch (org.json.JSONException e) {
         throw new org.json.JSONException("Not a valid RpcBodyLine", e);
@@ -248,7 +248,7 @@ public class RpcBodyLine implements codeprober.util.JsonUtil.ToJsonable {
       dst.writeUTF(((String)value));
       break;
     case arr:
-      codeprober.util.JsonUtil.<RpcBodyLine>writeDataArr(dst, ((java.util.List<RpcBodyLine>)value), ent2 -> ent2.writeTo(dst));
+      codeprober.util.JsonUtil.<RpcBodyLine>writeDataArr(dst, ((java.util.List<RpcBodyLine>)value), ent3 -> ent3.writeTo(dst));
       break;
     case node:
       ((NodeLocator)value).writeTo(dst);
